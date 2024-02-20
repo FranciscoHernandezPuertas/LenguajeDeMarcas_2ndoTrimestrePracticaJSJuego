c2da01a3637bf431202dfefb9ca360a3ad004793
Vicente <64921519+VicenteM23@users.noreply.github.com>
2024-02-20 19:26:36 +0100

    v1.0
    Primera versión estable del código JavaScript del juego

--- ahorcado.js
+++ ahorcado.js
@@ -1,24 +1,19 @@
-// ### VARIABLES ###
-
-// Array de palabras
+// Lista de palabras para el juego
 let palabras = [["atlantico", "Un océano"], ["ordenador", "Una máquina"], ["laurel", "Un árbol"], ["plaza", "Espacio público"], ["rueda", "Gran invento"], ["cereza", "Una fruta"], ["petanca", "Un juego"], ["higuera", "Un árbol"], ["everest", "Un monte"], ["relampago", "Antecede al trueno"], ["jirafa", "Un animal"], ["luxemburgo", "Un país"], ["uruguay", "Un país"], ["ilustracion", "Representación gráfica"], ["excursion", "Actividad en la naturaleza"], ["empanadilla", "De la panadería"], ["pastel", "De la pastelería"], ["colegio", "Lugar para estudiar"], ["carrera", "Competición"], ["mermelada", "Confitura"]];
-//opción SIN PISTA//
-//let palabras = ["atlantico","ordenador","laurel","plaza","rueda","cereza","petanca","higuera","everest","relampago","jirafa","luxemburgo","uruguay","ilustracion","excursion","empanadilla","pastel","colegio","carrera","mermelada"];
 
-// Palabra a averiguar
+// Palabra a adivinar
 let palabra = "";
 
 // Nº aleatorio
 let rand;
 
-//Array donde ir guardando las letras que acierta el usuario en la posición en la que están.
-//Puedes valerte de este array para ir creando con él el string que se va mostrando en los guiones a completar.
+// Letras utilizadas
 let oculta = [];
 
-// Elemento html donde se dibujan los guiones y el usuario va intentando completar la palabra
+// Elemento HTML donde se muestra la palabra a adivinar
 let hueco = document.getElementById("palabra");
 
-// Contador de intentos
+// Número de intentos
 let cont = 6;
 
 // Botones de letras
@@ -31,6 +26,22 @@ let reinicio;
 let pedirPista;
 let nuevaPalabra;
 
+// Elemento HTML donde se muestra el número de intentos restantes
+let intentosRestantes = document.getElementById("intentos");
+
+// Elemento HTML donde se muestra la imagen del ahorcado
+let imagenAhorcado = document.getElementById("imagen");
+
+// Elemento HTML donde se muestra el mensaje final
+let msgFinal = document.getElementById("msg-final");
+
+// Contenedor del abecedario
+let abecedarioContainer = document.getElementById("abcdario");
+
+//Elementos del código css para modificar las letras que estén o no
+var noEsta = document.querySelectorAll("abcdario");
+var siEsta = document.querySelectorAll("abcdario");
+
 // ### FUNCIONES ###
 
 function registrarPalabra(){
@@ -39,42 +50,63 @@ function registrarPalabra(){
 
 // Escoger palabra al azar
 function generaPalabra() {
-
+    palabra = palabras[Math.floor(Math.random() * palabras.length)][0];
+    hueco.textContent = "_".repeat(palabra.length);
 }
 
 // Función para dibujar los guiones de la palabra
 function pintarGuiones(num) {
-  for (var i = 0; i < num; i++) {
-    oculta[i] = "_";
-  }
-  hueco.innerHTML = oculta.join("");
+    for (var i = 0; i < num; i++) {
+        oculta[i] = "_";
+    }
+    hueco.innerHTML = oculta.join("");
 }
 
 //Función para crear los botones de las letras
-function generaABC (a,z) {
-  document.getElementById("abcdario").innerHTML = "";
-  let i = a.charCodeAt(0), j = z.charCodeAt(0);
-  let letra = "";
-  for( ; i<=j; i++) {
-    letra = String.fromCharCode(i).toUpperCase();
-    document.getElementById("abcdario").innerHTML += "<button value='" + letra + "' onclick='intento(\"" + letra + "\")' class='letra' id='"+letra+"'>" + letra + "</button>";
-    if(i==110) {
-      document.getElementById("abcdario").innerHTML += "<button value='Ñ' onclick='intento(\"Ñ\")' class='letra' id='Ñ'>Ñ</button>";
+function generaABC() {
+    for (let i = 0; i < 26; i++) {
+        let letra = String.fromCharCode(65 + i);
+        let boton = document.createElement("button");
+        boton.textContent = letra;
+        boton.addEventListener("click", function () {
+            if (!oculta.includes(letra.toLowerCase())) {
+                intento(letra.toLowerCase());
+            }
+        });
+        abecedarioContainer.appendChild(boton);
     }
-  }
+}
+
+// Función para actualizar la pantalla con la palabra mostrada y los intentos restantes
+function actualizarPantalla() {
+    intentosRestantes.textContent = cont;
+    imagenAhorcado.src = "img/ahorcado_" + cont + ".png";
 }
 
 //Función para comprobar la letra de cada intento
 function intento(letra) {
-  //acuérdate de deshabilitar la letra que acaba de ser pulsada
-
-
-
-  compruebaFin();
-  //esta función hace que la información Está!/No está! de cada letra desaparezca.
-  setTimeout(function () { 
-    document.getElementById("acierto").className = ""; 
-  }, 800);
+    if (palabra.includes(letra)) {
+        for (let i = 0; i < palabra.length; i++) {
+            if (palabra[i] === letra) {
+                let palabraArray = hueco.textContent.split("");
+                palabraArray[i] = letra;
+                hueco.textContent = palabraArray.join("");
+            }
+        }
+        noEsta.disabled = true;
+        siEsta.disabled = false; // Cambiar color del botón a verde
+    } else {
+        cont--;
+        noEsta.disabled = false;
+        siEsta.disabled = true; // Cambiar color del botón a rojo
+    }
+    oculta.push(letra);
+    actualizarPantalla();
+    compruebaFin();
+    //esta función hace que la información Está!/No está! de cada letra desaparezca.
+    setTimeout(function () {
+        document.getElementById("acierto").className = "";
+    }, 800);
 }
 
 //Función para mostrar la pista
@@ -84,38 +116,60 @@ function pista() {
 
 // Comprueba si ha finalizado
 function compruebaFin() {
-  if( oculta.indexOf("_") == -1 ) {
-    //poner Felicidades!! y asignar clases a juego ganado
-    
-    //se deshabilitan botones
-    for (var i = 0; i < buttons.length; i++) {
-      buttons[i].disabled = true;
+    if( oculta.indexOf("_") == -1 ) {
+      //poner Felicidades!! y asignar clases a juego ganado
+      
+      //se deshabilitan botones
+      for (var i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = true;
+      }
+      document.getElementById("reset").innerHTML = "Empezar";
+      btnInicio.onclick = function() { location.reload() };
+    }else if( cont == 0 ) {
+      //poner Game Over y asignar clases a juego perdido
+  
+      //crear elemento para mostrar la palabra que era y asignarle la clase encuadreR
+  
+      //se deshabilitan botones
+      for (var i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = true;
+      }
+      document.getElementById("reset").innerHTML = "Empezar";
+      btnInicio.onclick = function () { location.reload() };
     }
-    document.getElementById("reset").innerHTML = "Empezar";
-    btnInicio.onclick = function() { location.reload() };
-  }else if( cont == 0 ) {
-    //poner Game Over y asignar clases a juego perdido
-
-    //crear elemento para mostrar la palabra que era y asignarle la clase encuadreR
-
-    //se deshabilitan botones
-    for (var i = 0; i < buttons.length; i++) {
-      buttons[i].disabled = true;
-    }
-    document.getElementById("reset").innerHTML = "Empezar";
-    btnInicio.onclick = function () { location.reload() };
-  }
 }
+
+// Función para deshabilitar botones después de ganar o perder
+function deshabilitarBotones() {
+    let botones = document.querySelectorAll("#abcdario button");
+    botones.forEach(function (boton) {
+        boton.disabled = true;
+    });
+}
+
 
 // Restablecer juego
 function inicio() {
-  generaPalabra();
-  pintarGuiones(palabra.length);
-  generaABC("a","z");
-  cont = 6;
-  document.getElementById("intentos").innerHTML=cont;
-  document.getElementById("hueco-pista").innerHTML = "";
+    generaPalabra();
+    pintarGuiones(palabra.length);
+    cont = 6;
+    document.getElementById("intentos").innerHTML=cont;
+    document.getElementById("hueco-pista").innerHTML = "";
+    oculta = [];
+    msgFinal.textContent = "";
+    actualizarPantalla(); 
+    // Restablecer los botnes
+    let botones = document.querySelectorAll("#abcdario button");
+    botones.forEach(function (boton) {
+        boton.disabled = false;
+        boton.style.backgroundColor = "";
+    });
 }
+
+// Inicialización del juego
+generaPalabra();
+generaABC();
+actualizarPantalla();
 
 // Iniciar
 window.onload = inicio();
