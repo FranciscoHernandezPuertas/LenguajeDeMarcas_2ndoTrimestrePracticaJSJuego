4b0720aa119087a66f8eac55a417b05eb44f09d0
Vicente <64921519+VicenteM23@users.noreply.github.com>
2024-02-26 15:30:36 +0100

    v1.2(desesperación)
    Sáquenme de aquí

--- ahorcado.js
+++ ahorcado.js
@@ -1,9 +1,9 @@
 // ### VARIABLES ###
 
 // Array de palabras
-let palabras = [["atlantico", "Un océano"], ["ordenador", "Una máquina"], ["laurel", "Un árbol"], ["plaza", "Espacio público"], ["rueda", "Gran invento"], ["cereza", "Una fruta"], ["petanca", "Un juego"], ["higuera", "Un árbol"], ["everest", "Un monte"], ["relampago", "Antecede al trueno"], ["jirafa", "Un animal"], ["luxemburgo", "Un país"], ["uruguay", "Un país"], ["ilustracion", "Representación gráfica"], ["excursion", "Actividad en la naturaleza"], ["empanadilla", "De la panadería"], ["pastel", "De la pastelería"], ["colegio", "Lugar para estudiar"], ["carrera", "Competición"], ["mermelada", "Confitura"]];
+//let palabras = [["atlantico", "Un océano"], ["ordenador", "Una máquina"], ["laurel", "Un árbol"], ["plaza", "Espacio público"], ["rueda", "Gran invento"], ["cereza", "Una fruta"], ["petanca", "Un juego"], ["higuera", "Un árbol"], ["everest", "Un monte"], ["relampago", "Antecede al trueno"], ["jirafa", "Un animal"], ["luxemburgo", "Un país"], ["uruguay", "Un país"], ["ilustracion", "Representación gráfica"], ["excursion", "Actividad en la naturaleza"], ["empanadilla", "De la panadería"], ["pastel", "De la pastelería"], ["colegio", "Lugar para estudiar"], ["carrera", "Competición"], ["mermelada", "Confitura"]];
 //opción SIN PISTA//
-//let palabras = ["atlantico","ordenador","laurel","plaza","rueda","cereza","petanca","higuera","everest","relampago","jirafa","luxemburgo","uruguay","ilustracion","excursion","empanadilla","pastel","colegio","carrera","mermelada"];
+let palabras = ["atlantico","ordenador","laurel","plaza","rueda","cereza","petanca","higuera","everest","relampago","jirafa","luxemburgo","uruguay","ilustracion","excursion","empanadilla","pastel","colegio","carrera","mermelada"];
 
 // Palabra a averiguar
 let palabra = "";
@@ -44,8 +44,8 @@ let msgFinal = document.getElementById("msg-final");
 let abecedarioContainer = document.getElementById("abcdario");
 
 //Elementos del código css para modificar las letras que estén o no
-var noEsta = document.querySelectorAll("abcdario");
-var siEsta = document.querySelectorAll("abcdario");
+let noEsta = document.querySelectorAll("abcdario");
+let siEsta = document.querySelectorAll("abcdario");
 
 // ### FUNCIONES ###
 
@@ -55,7 +55,7 @@ function registrarPalabra() {
 
 // Escoger palabra al azar
 function generaPalabra() {
-  palabra = palabras[Math.floor(Math.random() * palabras.length)][0];
+  palabra = palabras[Math.floor(Math.random() * palabras.length)];
   hueco.textContent = "_".repeat(palabra.length);
 }
 
@@ -69,7 +69,7 @@ function pintarGuiones(num) {
 
 //Función para crear los botones de las letras
 function generaABC() {
-  for (let i = 0; i < 26; i++) {
+  for (var i = 0; i < 26; i++) {
     let letra = String.fromCharCode(65 + i);
     let boton = document.createElement("button");
     boton.textContent = letra;
@@ -91,19 +91,19 @@ function actualizarPantalla() {
 //Función para comprobar la letra de cada intento
 function intento(letra) {
   if (palabra.includes(letra)) {
-    for (let i = 0; i < palabra.length; i++) {
+    for (var i = 0; i < palabra.length; i++) {
       if (palabra[i] === letra) {
         let palabraArray = hueco.textContent.split("");
         palabraArray[i] = letra;
         hueco.textContent = palabraArray.join("");
       }
     }
-    noEsta.disabled = true;
-    siEsta.disabled = false; // Cambiar color del botón a verde
+    let letraEsta = document.getElementById("abcdario");
+    letraEsta.classList.add("siEsta:disabled");
   } else {
     cont--;
-    noEsta.disabled = false;
-    siEsta.disabled = true; // Cambiar color del botón a rojo
+    let letraNoEsta = document.getElementById("abcdario");
+    letraNoEsta.classList.add("noEsta:disabled");
   }
   oculta.push(letra);
   actualizarPantalla();
@@ -114,31 +114,41 @@ function intento(letra) {
   }, 800);
 }
 
+// Función para obtener la segunda palabra del array bidimensional
+function obtenerSegundaPalabra() {
+  return palabras.map(function(elemento) {
+      return elemento[1];
+  });
+}
 //Función para mostrar la pista
 function pista() {
-  //OPCIONAL//
+  document.getElementById("pista").addEventListener("click", function() {
+    let segundasPalabras = obtenerSegundaPalabra();
+    alert(segundasPalabras.join("\n"));
+  });
 }
 
 // Comprueba si ha finalizado
 function compruebaFin() {
-  if (oculta.indexOf("_") == -1) {
+  if ( oculta.indexOf("_") == -1 ) {
     //poner Felicidades!! y asignar clases a juego ganado
-
+    document.getElementById("msg-final").innerHTML = "Felicidades!!";
+    document.getElementById("msg-final").classList.add("ganado");
     //se deshabilitan botones
-    for (var i = 0; i < buttons.length; i++) {
-      buttons[i].disabled = true;
-    }
-    document.getElementById("reset").innerHTML = "Empezar";
+    deshabilitarBotones();
+    document.getElementById("reset").innerHTML = "Reiniciar";
     btnInicio.onclick = function () { location.reload() };
   } else if (cont == 0) {
     //poner Game Over y asignar clases a juego perdido
-
+    document.getElementById("msg-final").innerHTML = "Game Over";
+    document.getElementById("msg-final").classList.add("msg-final");
     //crear elemento para mostrar la palabra que era y asignarle la clase encuadreR
-
+    var palabraOcultaElement = document.createElement("div");
+    palabraOcultaElement.innerHTML = "La palabra era: " + palabra;
+    palabraOcultaElement.classList.add("encuadreR");
+    document.body.appendChild(palabraOcultaElement);
     //se deshabilitan botones
-    for (var i = 0; i < buttons.length; i++) {
-      buttons[i].disabled = true;
-    }
+    deshabilitarBotones();
     document.getElementById("reset").innerHTML = "Empezar";
     btnInicio.onclick = function () { location.reload() };
   }
@@ -146,10 +156,12 @@ function compruebaFin() {
 
 // Función para deshabilitar botones después de ganar o perder
 function deshabilitarBotones() {
-  let botones = document.querySelectorAll("#abcdario button");
-  botones.forEach(function (boton) {
-    boton.disabled = true;
-  });
+  if ( cont == 0 ) {
+    let botones = document.querySelectorAll("#abcdario button");
+    botones.forEach(function (boton) {
+      boton.disabled = true;
+    });
+  }
 }
 
 
@@ -177,7 +189,11 @@ generaABC();
 actualizarPantalla();
 
 // Iniciar
-window.onload = inicio();
+window.onload = function() {
+  inicio();
+  reinicio = document.getElementById('reset');
+  reinicio.addEventListener('click', inicio);
+};
 reinicio = document.getElementById('reset');
 reinicio.addEventListener('click', inicio);
 pedirPista = document.getElementById('pista');
